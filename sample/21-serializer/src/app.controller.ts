9314788f329471246fd9f1246274259c7deb1e97
yongdokim <yongdo.kim@seoulmafia.com>
2021-05-03 13:36:51 +0900

    adding commnet 21

--- sample/21-serializer/src/app.controller.ts
+++ sample/21-serializer/src/app.controller.ts
@@ -8,14 +8,14 @@ import { RoleEntity } from './entities/role.entity';
 import { UserEntity } from './entities/user.entity';
 
 @Controller()
-@UseInterceptors(ClassSerializerInterceptor)
+@UseInterceptors(ClassSerializerInterceptor) //인터셉터, 직렬화 
 export class AppController {
   @Get()
   findOne(): UserEntity {
     return new UserEntity({
       id: 1,
-      firstName: 'Kamil',
-      lastName: 'Mysliwiec',
+      firstName: 'Kamil2',
+      lastName: 'Mysliwiec3',
       password: 'password',
       role: new RoleEntity({ id: 1, name: 'admin' }),
     });
--- sample/21-serializer/src/entities/user.entity.ts
+++ sample/21-serializer/src/entities/user.entity.ts
@@ -9,14 +9,28 @@ export class UserEntity {
   @Exclude()
   password: string;
 
+
+  //@Expose는 return 값에 없어도 추가해서 준다. 
+  //"fullName": "Kamil2 Mysliwiec3",
   @Expose()
   get fullName(): string {
     return `${this.firstName} ${this.lastName}`;
   }
 
+  //  "testFunction": "this is as test function what i have to do now"
+  @Expose()
+  get testFunction(): string {
+    return 'this is as test function what i have to do now';
+  }
+
+
+  //RoleEntity의 일부만을 가져오고 싶은 경우 @Transform을 사용
+  //value.name -> "role":"admin"
+  //value.id -> "role":"1"
   @Transform(({ value }) => value.name)
   role: RoleEntity;
 
+  //생성자에서 받아주는 곳 
   constructor(partial: Partial<UserEntity>) {
     Object.assign(this, partial);
   }
